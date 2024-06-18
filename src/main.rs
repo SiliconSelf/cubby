7004adc079efb994fffd08f4baef343f2d16eb76
Wren Morozova <siliconself@proton.me>
2024-06-18 00:02:04 -0600

    Created basic axum app for testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 #![doc = include_str!("../README.md")]
 
-fn main() {
-    println!("Hello, world!");
+#[cfg(all(not(target_env = "msvc"), feature = "jemalloc"))]
+use tikv_jemallocator::Jemalloc;
+#[cfg(all(not(target_env = "msvc"), feature = "jemalloc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
+use axum::{
+    routing::get,
+    Router
+};
+
+#[tokio::main]
+async fn main() {
+    // Initialize logging
+    tracing_subscriber::fmt::init();
+    // Create basic app
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }));
+    // Create listener
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await
+        .expect("Failed to start listener");
+    axum::serve(listener, app).await
+        .expect("Failed to serve axum app");
 }
