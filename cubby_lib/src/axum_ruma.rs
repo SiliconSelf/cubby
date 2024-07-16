5c3ea3a4819208f474f687cab459d846b8560f23
Wren Morozova <siliconself@proton.me>
2024-07-15 18:39:47 -0600

    Changes to RumaResponder
    This commit adds a new variant to the `RumaResponder` enum, `OneOff`. This variant allows for custom responses that don't fit the typical matrix error structure to be returned from endpoints with an associated status code.
    
    For an example of such a response body, refer to the 401 response for the /v3/register endpoint in the client-server API
    
    https://spec.matrix.org/latest/client-server-api/#post_matrixclientv3register

--- cubby_lib/src/axum_ruma.rs
+++ cubby_lib/src/axum_ruma.rs
@@ -60,6 +60,11 @@ pub enum RumaResponder<T, E> {
     Ok(T),
     /// Some error occured
     Err(E),
+    /// Something that isn't Ok, but also doesn't implement IntoMatrixError
+    /// 
+    /// This is mostly once off error types such as the 401 response for
+    /// /v3/register ([spec](https://spec.matrix.org/latest/client-server-api/#post_matrixclientv3register))
+    OneOff((StatusCode, serde_json::Value))
 }
 
 impl<T, E> IntoResponse for RumaResponder<T, E>
@@ -68,14 +73,26 @@ where
     E: IntoMatrixError,
 {
     fn into_response(self) -> Response {
-        let Ok(res) = (match self {
-            RumaResponder::Ok(t) => t.try_into_http_response::<BytesMut>(),
+        match self {
+            RumaResponder::Ok(t) => {
+                let Ok(body) = t.try_into_http_response::<BytesMut>() else {
+                    return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+                };
+                body.map(BytesMut::freeze).map(Body::from).into_response()
+            },
             RumaResponder::Err(e) => {
-                e.into_matrix_error().try_into_http_response::<BytesMut>()
-            }
-        }) else {
-            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
-        };
-        res.map(BytesMut::freeze).map(Body::from).into_response()
+                let Ok(body) = e.into_matrix_error().try_into_http_response::<BytesMut>() else {
+                    return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+                };
+                body.map(BytesMut::freeze).map(Body::from).into_response()
+            },
+            RumaResponder::OneOff((c, v)) => {
+                Response::builder()
+                    .status(c)
+                    .header("content-type", "application/json")
+                    .body(Body::from(v.to_string()))
+                    .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR.into_response())
+            },
+        }
     }
 }
