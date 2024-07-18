8bf0274a9fa6205c131d0289c146f4cc43799577
Wren Morozova <siliconself@proton.me>
2024-07-17 21:52:58 -0600

    Began rewrite of dataframe manager

--- cubby_server/src/managers/dataframes.rs
+++ cubby_server/src/managers/dataframes.rs
@@ -12,7 +12,7 @@ use std::{
     time::Duration,
 };
 
-use crossbeam_channel::{unbounded, RecvTimeoutError, Sender};
+use crossbeam_channel::{unbounded, Receiver, RecvTimeoutError, Sender};
 use once_cell::sync::Lazy;
 use polars::prelude::*;
 use tokio::sync::oneshot;
@@ -36,232 +36,78 @@ static TEMPLATE_FRAME: Lazy<Vec<u8>> = Lazy::new(|| {
     buffer
 });
 
-/// This static exists to manage all the file locks held by the program at
-/// any given time.
-///
-/// See the documentation for ``LockManager`` for more details.
-static LOCK_MANAGER: Lazy<LockManager> = Lazy::new(LockManager::new);
+trait Message {}
 
-/// Eagerly manages a series of locks under its control.
-///
-/// The primary method of interaction with the `LockManager` is through the
-/// ``get_lock()`` method where, when given a file path, the manager will return
-/// a future that can be `await`ed until a lock is achieved on the file. Once
-/// the `FileLock` is dropped, it will send a message back to the manager saying
-/// it has been dropped and to unlock the path it was locking for the next
-/// request in line.
-///
-/// Requests for file locks are in a first-come-first-serve basis. When freeing
-/// locks, the manager will eagerly process all pending `FileLock` drop messages
-/// until none remain in its channel. It will then iterate through the entire
-/// queue of requests for locks and issue as many as possible until all
-/// requested files are locked or until there are no more requests in the
-/// queue.
-struct LockManager {
-    /// The internal transmitter for sending requests for locks to the detached
-    /// thread.
-    manager_tx: Sender<(PathBuf, oneshot::Sender<FileLock>)>,
+pub(crate) struct DataFrameManager {
+    tx: Sender<Box<dyn Message>>,
+    rx: Receiver<Box<dyn Message>>
 }
 
-impl LockManager {
-    /// Request a lock on a specific file
-    async fn get_lock<P: Into<PathBuf>>(&self, path: P) -> FileLock {
-        let (tx, rx) = oneshot::channel();
-        self.manager_tx
-            .send((path.into(), tx))
-            .expect("Channel communication failed");
-        rx.into_future().await.expect("Channel communication failed")
-    }
-
-    /// Create a new `LockManager`. Realistically, this should only be called
-    /// once while creating the static `LOCK_MANAGER`.
-    fn new() -> Self {
-        // Create channels to the other thread
-        let (manager_tx, manager_rx) =
-            unbounded::<(PathBuf, oneshot::Sender<FileLock>)>();
-        // Spawn the task
-        tokio::spawn(async move {
-            let (lock_tx, lock_rx) = unbounded::<PathBuf>();
-            let mut locks: HashSet<PathBuf> = HashSet::new();
-            let mut queue: HashMap<
-                PathBuf,
-                VecDeque<oneshot::Sender<FileLock>>,
-            > = HashMap::new();
-            loop {
-                // Eagerly release all pending locks
-                loop {
-                    match lock_rx.recv_timeout(Duration::from_millis(1)) {
-                        Err(RecvTimeoutError::Timeout) => {
-                            // All the pending locks have been released, move on
-                            // to issuing new ones
-                            break;
-                        }
-                        Err(RecvTimeoutError::Disconnected) => {
-                            panic!("Lock Manager became disconnected");
-                        }
-                        // A lock has been released
-                        Ok(m) => {
-                            trace!("Unlocking {m:?}");
-                            locks.remove(&m);
-                        }
-                    };
-                }
-                // Queue all pending requests for locks
-                loop {
-                    match manager_rx.recv_timeout(Duration::from_millis(1)) {
-                        Err(RecvTimeoutError::Timeout) => {
-                            // Nothing more to add to the queue
-                            break;
-                        }
-                        Err(RecvTimeoutError::Disconnected) => {
-                            panic!("Lock Manager became disconnected");
-                        }
-                        // A lock has been requested
-                        Ok((p, s)) => {
-                            trace!(
-                                "Adding request for file to the queue: {p:?}"
-                            );
-                            if let Some(q) = queue.get_mut(&p) {
-                                q.push_back(s);
-                            } else {
-                                let mut new_queue = VecDeque::new();
-                                new_queue.push_back(s);
-                                queue.insert(p, new_queue);
-                            }
-                        }
-                    };
-                }
-                // Assign new locks based on any demand
-                if !queue.is_empty() {
-                    for (k, v) in &mut queue {
-                        // Skip ahead to the next file if this one is locked
-                        if locks.contains(k) {
-                            continue;
-                        }
-                        // Issue a new lock
-                        trace!("Locking {k:?}");
-                        locks.insert(k.clone());
-                        let lock = FileLock {
-                            path: k.to_owned(),
-                            channel: lock_tx.clone(),
-                        };
-                        v.pop_front()
-                            .expect("We already checked that this isn't empty")
-                            .send(lock)
-                            .expect("Failed to send new file lock");
-                    }
-                }
-            }
-        });
+impl DataFrameManager {
+    pub(crate) fn new() -> Self {
+        let (_thread_tx, manager_rx) = unbounded::<Box<dyn Message>>();
+        let (manager_tx, _thread_rx) = unbounded::<Box<dyn Message>>();
+        tokio::spawn(async move { todo!(); });
         Self {
-            manager_tx,
+            tx: manager_tx,
+            rx: manager_rx
         }
     }
 }
 
-/// Represents a lock on a specific file
-///
-/// When dropped, this struct will send a message back to the lock manager that
-/// issued it.
+trait Receive<T> {
+    fn handle(message: T);
+}
+
 #[derive(Debug)]
-struct FileLock {
-    /// The file path is lock represents
-    path: PathBuf,
-    /// The channel back to the sender that issued the lock
-    channel: Sender<PathBuf>,
+struct GetLockMessage<P> where P: Into<PathBuf> {
+    path: P
 }
 
-impl Drop for FileLock {
+impl<P> Receive<GetLockMessage<P>> for DataFrameManager where P: Into<PathBuf> + Debug {
     #[instrument(level = "trace")]
+    fn handle(message: GetLockMessage<P>) {
+        let _path: PathBuf = message.path.into();
+        todo!();
+    }
+}
+
+/// A wrapper around a given `LazyFrame`.
+///
+/// This struct has a custom Drop implementation that will send the current
+/// contents back to the `DataframeManager` that created it to write changes to
+/// disk.
+pub(crate) struct ManagedLazyFrame {
+    /// The internal `LazyFrame`
+    frame: LazyFrame,
+    /// The lock on the file, this is just here to prevent any other writers
+    /// from getting a lock on the file while this is in use
+    _file_lock: FileLock,
+    /// The transmitter for sending the internal `frame` back to the manager
+    tx: Sender<LazyFrame>,
+}
+
+impl ManagedLazyFrame {
+    /// Run a closure taking the internal `LazyFrame` as an argument, replacing
+    /// the internal frame with its result
+    ///
+    /// This function exists because pretty much all of the methods for
+    /// `DataFrame` an`LazyFrame`me have the `fn x(self, ..) -> Self` function
+    /// signature pattern, meaning it's basically impossible to use references
+    /// in any productive way and any data processing at endpoints would require
+    /// a large number of clones and a convoluted mess of channels. With this
+    /// approach, we bring the function to the data because we can't bring the
+    /// data to the function.
+    pub(crate) fn apply<F: FnOnce(LazyFrame) -> LazyFrame>(mut self, closure: F) {
+        self.frame = closure(self.frame.clone());
+    }
+}
+
+impl Drop for ManagedLazyFrame {
+    #[instrument(level = "trace", skip(self))]
     fn drop(&mut self) {
-        trace!("Dropping lock for {:?}", self.path);
-        self.channel
-            .send(self.path.clone())
-            .expect("Failed to send message to unlock my path");
+        trace!("Attempting to send LazyFrame back to manager during drop");
+        self.tx
+            .send(self.frame.clone());
     }
-}
-
-/// Manages the in-use `DataFrames`.
-///
-/// This struct should only exist once as part of the state being managed by the
-/// axum router
-#[derive(Clone, Debug)]
-pub(crate) struct DataframeManager;
-
-impl DataframeManager {
-    /// Create a new `DataframeManager`
-    pub(crate) fn new() -> Self {
-        Self {}
-    }
-
-    /// Retrieve a `LazyFrame` from the cache, inserting it into the cache if it
-    /// does not already exist.
-    ///
-    /// Paths provided to this function are relative to the configured
-    /// `data_path`
-    ///
-    /// This function is intended for read-only access to parquet data. For
-    /// write access, please use `get_write` to take advantage of extra sync
-    /// protections.
-    #[allow(clippy::unused_self)]
-    #[instrument(level = "trace")]
-    pub(crate) fn get_lazy<P: Into<PathBuf> + Debug>(
-        &self,
-        path: P,
-    ) -> LazyFrame {
-        scan_file(path)
-    }
-
-    /// Returns an eager `DataFrame` suitable for writing
-    #[allow(clippy::unused_self)]
-    #[instrument(level = "trace")]
-    pub(crate) fn get_write<P: Into<PathBuf> + Debug>(
-        &self,
-        path: P,
-    ) -> (DataFrame, Sender<DataFrame>) {
-        let mut key = PROGRAM_CONFIG.data_path.clone();
-        key.push(path.into());
-        let scan = scan_file(&key).collect().expect("Failed to scan file");
-        let (tx, rx) = unbounded::<DataFrame>();
-        tokio::spawn(async move {
-            // Block until we receive the new data to write
-            let Ok(mut value) = rx.recv() else {
-                error!("Receiving {key:?} failed! Did the endpoint give up?");
-                return;
-            };
-            trace!("Received returned LazyFrame for {key:?}");
-            // Mom said it's my turn on the Mutex
-            let _file_lock = LOCK_MANAGER.get_lock(&key);
-            // Write new data to path
-            let mut file = File::create(&key).expect("Failed to create file");
-            ParquetWriter::new(&mut file)
-                .finish(&mut value)
-                .expect("Failed to write file");
-            trace!("File written");
-        });
-        (scan, tx)
-    }
-}
-
-/// Scan a parquet file on disk, creating it from the `TEMPLATE_FRAME` if it
-/// does not already exist.
-///
-/// Paths provided to this function are relative to the configured
-/// `PROGRAM_CONFIG.data_path`.
-#[instrument(level = "trace")]
-fn scan_file<P: Into<PathBuf> + Debug>(path: P) -> LazyFrame {
-    let mut key = PROGRAM_CONFIG.data_path.clone();
-    key.push(path.into());
-    trace!("Scanning parquet file {key:?}");
-    if !key.is_file() {
-        debug!("Creating new parquet file: {key:?}");
-        let mut file =
-            File::create(&key).expect("Failed to create new parquet file");
-        file.write_all(&TEMPLATE_FRAME)
-            .expect("Failed to write template to new parquet file");
-    }
-    let scan_args = ScanArgsParquet::default();
-
-    LazyFrame::scan_parquet(key, scan_args)
-        .expect("Failed to scan parquet file")
 }
